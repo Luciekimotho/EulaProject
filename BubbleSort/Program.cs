60061d2cf1354324c06129d5c6974cc426052a7c
Luciekimotho <luciekimotho@gmail.com>
2018-11-30 16:51:57 +0300

    Bubble sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 800, 11, 50, 771, 649, 770, 240, 9 };
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j+1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            for (int sort = 0; sort < arr.Length; sort++)
+            {
+                Console.Write("{0}\t", arr[sort]);
+            }
+
+
+
+        }
+    }
+}
